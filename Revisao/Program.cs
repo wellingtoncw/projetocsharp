e583a2895c1c4891cbec62aaf660b1507ce50981
wellingtoncw <wellingtoncw7@gmail.com>
2021-03-23 20:37:14 -0300

    cria projeto Revisao
    cria projeto console revisão

---
+++ Revisao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno;");
+            Console.WriteLine("2- Listar alunos;");
+            Console.WriteLine("3- Calcular média geral.");
+        }
+    }
+}
